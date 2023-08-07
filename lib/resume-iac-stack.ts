10e7da0595266916c55dfe4f45de1595552226a8
György Balássy <balassy@users.noreply.github.com>
2023-08-07 07:36:35 +0200

    feat: rename bucket (#6)

--- lib/resume-iac-stack.ts
+++ lib/resume-iac-stack.ts
@@ -6,8 +6,8 @@ export class ResumeIacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'resume-bucket-cdk', {
-      bucketName: 'balassy-resume-site'
+    new s3.Bucket(this, 'my-bucket-cdk', {
+      bucketName: 'my-resume-site'
     });
   }
 }
