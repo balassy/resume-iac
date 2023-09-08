0536401ed7162b971026caec5ed9baee7fc2bfab
György Balássy <balassy@users.noreply.github.com>
2023-09-08 17:40:10 +0200

    feat: redirect to default document in CloudFront

--- lib/frontend/resume-frontend.ts
+++ lib/frontend/resume-frontend.ts
@@ -2,11 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket, IBucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import { AllowedMethods, Distribution, IDistribution, IOrigin, OriginAccessIdentity, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { AllowedMethods, Distribution, Function, FunctionCode, FunctionEventType, IDistribution, IFunction, IOrigin, OriginAccessIdentity, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
 import { Certificate, ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { ARecord, AaaaRecord, HostedZone, IHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 import { IResumeFrontendParams } from './resume-frontend.types';
+import path = require('path');
 
 export interface IResumeFrontendProps extends IResumeFrontendParams {
 };
@@ -21,7 +22,8 @@ export class ResumeFrontend extends Construct {
 
     const certificate: ICertificate = this.findCertificate(props.certificateArn);
 
-    const cloudFrontDistribution:IDistribution = this.createCloudFrontDistribution(s3Origin, certificate, [props.domainName, props.domainAlias]);
+    const cloudFrontDefaultDocumentHandler: IFunction = this.createCloudFrontFunction();
+    const cloudFrontDistribution:IDistribution = this.createCloudFrontDistribution(s3Origin, certificate, [props.domainName, props.domainAlias], cloudFrontDefaultDocumentHandler);
 
     const hostedZone: IHostedZone = this.findHostedZone(props.hostedZoneName, props.hostedZoneId);
 
@@ -42,12 +44,27 @@ export class ResumeFrontend extends Construct {
     });
   }
 
-  private createCloudFrontDistribution(origin: IOrigin, certificate: ICertificate, domainNames: string[]): IDistribution {
+  private createCloudFrontFunction(): IFunction {
+    return new Function(this, 'ResumeFrontendDistributionDefaultDocHandler', {
+      code: FunctionCode.fromFile({
+        filePath: path.join(__dirname, 'cloudfront-redirect-handler.js')
+      }),
+      comment: 'Adds index.html to the end of directory paths.'
+    });
+  }
+
+  private createCloudFrontDistribution(origin: IOrigin, certificate: ICertificate, domainNames: string[], requestHandler: IFunction): IDistribution {
     return new Distribution(this, 'ResumeFrontendDistribution', {
       defaultBehavior: {
         origin,
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS, 
+        functionAssociations: [
+          {
+            function: requestHandler,
+            eventType: FunctionEventType.VIEWER_REQUEST
+          }
+        ]
       },
       defaultRootObject: 'index.html',
       domainNames,
