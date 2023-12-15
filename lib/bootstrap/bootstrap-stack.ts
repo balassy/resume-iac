aa908af0b1f20082e0c2451b597769aa7ed3342a
György Balássy <balassy@users.noreply.github.com>
2023-12-15 16:01:37 +0100

    feat: create iam policy, role and identity provider from the bootstrap stack (#33)

--- lib/bootstrap/bootstrap-stack.ts
+++ lib/bootstrap/bootstrap-stack.ts
@@ -2,12 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ResumeFrontendConfigurator } from '../frontend/resume-frontend-configurator';
 
-
 export class BootstrapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
     
+    const gitHubUserName = (new cdk.CfnParameter(this, 'gitHubUserName')).valueAsString;
+    const gitHubRepoName = (new cdk.CfnParameter(this, 'gitHubRepoName')).valueAsString;
+
     const frontendConfigurator = new ResumeFrontendConfigurator(this, 'ResumeFrontendConfig');
     frontendConfigurator.createParamsWithDefaults();
+    frontendConfigurator.createAccessPermissions(gitHubUserName, gitHubRepoName);
+
+    new cdk.CfnOutput(this, 'ResumeOutputAwsRegion', {
+      value: this.region
+    }).overrideLogicalId('AwsRegion');
   }
 }
