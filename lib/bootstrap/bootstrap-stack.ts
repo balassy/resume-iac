8351fec9c53ef0083e879e972a3e4559cc43cfaa
György Balássy <balassy@users.noreply.github.com>
2023-12-15 14:00:19 +0100

    create IAM role, policy and identity provider for IaC deployment

--- lib/bootstrap/bootstrap-stack.ts
+++ lib/bootstrap/bootstrap-stack.ts
@@ -7,7 +7,11 @@ export class BootstrapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
     
+    const gitHubUserName = (new cdk.CfnParameter(this, 'gitHubUserName')).valueAsString;
+    const gitHubRepoName = (new cdk.CfnParameter(this, 'gitHubRepoName')).valueAsString;
+
     const frontendConfigurator = new ResumeFrontendConfigurator(this, 'ResumeFrontendConfig');
     frontendConfigurator.createParamsWithDefaults();
+    frontendConfigurator.createAccessPermissions(gitHubUserName, gitHubRepoName);
   }
 }
--- lib/frontend/resume-frontend-configurator.ts
+++ lib/frontend/resume-frontend-configurator.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import { Effect, IOpenIdConnectProvider, ManagedPolicy, OpenIdConnectProvider, PolicyStatement, Role, WebIdentityPrincipal } from 'aws-cdk-lib/aws-iam';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { NagSuppressions } from 'cdk-nag';
 import { Construct } from "constructs";
 import { IResumeFrontendParams } from './resume-frontend.types';
 
@@ -32,5 +34,60 @@ export class ResumeFrontendConfigurator extends Construct {
     new StringParameter(this, 'ResumeFrontendParamCertificateArn', { parameterName: ParamNames.CERTIFICATE_ARN, stringValue: 'arn:aws:acm:us-east-1:...' });
     new StringParameter(this, 'ResumeFrontendParamHostedZoneName', { parameterName: ParamNames.HOSTEDZONE_NAME, stringValue: 'example.com' });
     new StringParameter(this, 'ResumeFrontendParamHostedZoneId', { parameterName: ParamNames.HOSTEDZONE_ID, stringValue: 'Z04057...' });
+  }
+
+  public createAccessPermissions(gitHubUserName: string, gitHubRepoName: string) {
+    const gitHubIdentityProvider = new OpenIdConnectProvider(this, 'ResumeGitHubIdentityProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: [ // aka. Audiences
+        'sts.amazonaws.com'
+      ]
+    });
+
+    this.createAccessPermissionsForIaCUpdate(gitHubIdentityProvider, gitHubUserName, gitHubRepoName);
+    this.createAccessPermissionsForFrontendUpdate();
+  }
+
+  private createAccessPermissionsForIaCUpdate(identityProvider: IOpenIdConnectProvider, gitHubUserName: string, gitHubRepoName: string) {
+    const managedPolicy = new ManagedPolicy(this, 'ResumeIaCDeployPolicy', {
+      description: 'Permissions required to IaC update from CDK.',
+      statements: [new PolicyStatement({
+        actions: [
+          'sts:AssumeRole',
+        ],
+        resources: [
+          'arn:aws:iam::*:role/cdk-*'
+        ],
+        effect: Effect.ALLOW
+      })],
+    });
+
+    const principal = new WebIdentityPrincipal(
+      identityProvider.openIdConnectProviderArn,
+      {
+        StringEquals: {
+          'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com'
+        },
+        StringLike: {
+          'token.actions.githubusercontent.com:sub': `repo:${gitHubUserName}/${gitHubRepoName}:*`
+        }
+      }
+    );   
+
+    const role = new Role(this, 'ResumeIaCDeployer', {
+      description: 'The permissions assigned to the GitHub Actions workflow.',
+      assumedBy: principal,
+    });
+
+    role.addManagedPolicy(managedPolicy);
+
+    NagSuppressions.addResourceSuppressions(managedPolicy, [{
+      id: 'AwsSolutions-IAM5',
+      reason: 'Access is granted to perform all CDK operations.'
+    }]);
+  }
+
+  private createAccessPermissionsForFrontendUpdate() {
+
   }
 }
