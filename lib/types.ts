53bced268e7a7dbafbdd027f0c642c8a9bfd43bc
György Balássy <balassy@users.noreply.github.com>
2023-12-27 17:46:01 +0100

    feat: create IAM policy and role for frontend upload (#34)

---
+++ lib/types.ts
@@ -0,0 +1,11 @@
+// TODO: Use "nominal typing" instead of type aliases:
+// https://github.com/Microsoft/TypeScript/blob/7b48a182c05ea4dea81bab73ecbbe9e013a79e99/src/compiler/types.ts#L693-L698
+// https://michalzalecki.com/nominal-typing-in-typescript/
+// https://basarat.gitbook.io/typescript/main-1/nominaltyping
+
+export type Arn = string;
+export type Id = string;
+export type AccountId = string;
+
+export type GitHubRepoName = string;
+export type GitHubUserName = string;
